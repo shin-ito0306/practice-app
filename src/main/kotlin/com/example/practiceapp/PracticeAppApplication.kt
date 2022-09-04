53e26cf749566eacca2a21d4f79c0ccaf68ad819
shin-ito0306 <shin.20000306@icloud.com>
2022-09-04 18:12:47 +0900

    initiarizerの設定

---
+++ src/main/kotlin/com/example/practiceapp/PracticeAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.practiceapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/practiceapp/PracticeAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.practiceapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
